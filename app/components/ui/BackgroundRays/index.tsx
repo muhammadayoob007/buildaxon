4399dd9d5a032ed18e8e0bed5bde5c06457b00b8
muhammadayoob007 <muhammad.ayoob007@gmail.com>
2026-09-09 08:35:28 +0800

    add bottom-right teal glow

--- app/components/ui/BackgroundRays/index.tsx
+++ app/components/ui/BackgroundRays/index.tsx
@@ -10,7 +10,8 @@ const BackgroundRays = () => {
       <div className={`${styles.lightRay} ${styles.ray5}`}></div>
       <div className={`${styles.lightRay} ${styles.ray6}`}></div>
       <div className={`${styles.lightRay} ${styles.ray7}`}></div>
-      <div className={`${styles.lightRay} ${styles.ray8}`}></div>
+      <div className={`${styles.lightRay} ${styles.ray9}`}></div>
+      <div className={`${styles.lightRay} ${styles.ray10}`}></div>
     </div>
   );
 };
